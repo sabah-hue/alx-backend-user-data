f7fdefa24e0e07d419d65e68230d5cd31d6f5bdf
sabah-hue <sabah.abdelbaset@gmail.com>
2024-08-30 01:50:37 +0300

    output issue

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -56,13 +56,13 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """ Connect to secure database """
-    username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
-    password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.environ.get("PERSONAL_DATA_DB_NAME")
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
 
-    x = mysql.connector.connection.MySQLConnection(user=username,
-                                                   password=password,
-                                                   host=host,
-                                                   database=db_name)
+    x = mysql.connector.connect(user=username,
+                                password=password,
+                                host=host,
+                                database=db_name)
     return x

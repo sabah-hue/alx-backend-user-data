381a392fee6bd83ee921fc1c8e12801ac9037a2f
sabah-hue <sabah.abdelbaset@gmail.com>
2024-09-07 05:30:39 +0300

    debug

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python3
-from flask import request
-""" Module of Auth
+"""Auth module
 """
+from flask import request
+from typing import List, TypeVar
+import fnmatch
 
 
 class Auth:
-    """
-    auth class
+    """Auth class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ check auth is required
         """
-        require path
-        """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
+        """ auth header
         """
-        return flask object
-        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ return flask object """
+        """ get current user
+        """
         return None

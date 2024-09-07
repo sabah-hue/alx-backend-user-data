3f6232285f223e9c25c0d9f913fa2698a58b08d2
sabah-hue <sabah.abdelbaset@gmail.com>
2024-09-07 05:07:22 +0300

    add auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Module of Auth
+"""
+import request from flask
+
+
+class Auth:
+    """ auth class """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require path """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ return flask object"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ return flask object """
+        return None

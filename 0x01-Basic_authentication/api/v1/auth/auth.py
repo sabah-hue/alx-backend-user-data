be73ae7d8da58f92d42cc27dce67719ad8c62549
sabah-hue <sabah.abdelbaset@gmail.com>
2024-09-07 05:36:19 +0300

    output issue

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,8 +18,11 @@ class Auth:
         if excluded_paths is None or not excluded_paths:
             return True
 
+        path = path.rstrip("/")
         for excluded_path in excluded_paths:
-            if fnmatch.fnmatch(path, excluded_path):
+            # if fnmatch.fnmatch(path, excluded_path):
+            #     return False
+            if path == excluded_path.rstrip("/"):
                 return False
 
         return True

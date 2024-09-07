641e29e7639920b6bfdf3329fb6c6860503c30a3
sabah-hue <sabah.abdelbaset@gmail.com>
2024-09-07 04:57:33 +0300

    debug

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,8 +26,8 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+def unauthorized() -> None:
     """
     GET /api/v1/unauthorized
     Return: Custom Error Pages
@@ -35,8 +35,8 @@ def unauthorized() -> str:
     return abort(401)
 
 
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden() -> str:
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+def forbidden() -> None:
     """ GET /api/v1/forbidden
     Return:
     - Custom Error Pages

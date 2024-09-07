648db52573362accbf284f93650f59b68e340084
sabah-hue <sabah.abdelbaset@gmail.com>
2024-09-07 05:57:37 +0300

    dd

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,13 @@ from .auth import Auth
 class BasicAuth(Auth):
     """Basic auth class
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """base64"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split("Basic ")[1].strip()
